cc887b27cafb665e4a63bbe3883b0a8555350c7a
Manuj Vashist <manujvashist@gmail.com>
2017-05-07 05:37:09 +0530

    splash screen added

--- app/src/main/java/com/Infiniteloop/TogetherPlay/MainActivity.java
+++ app/src/main/java/com/Infiniteloop/TogetherPlay/MainActivity.java
@@ -7,6 +7,7 @@ import hw.infloop.tp1.Main2Activity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         button = (Button) findViewById(R.id.btn_Mic);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/Infiniteloop/TogetherPlay/splashActivity.java
@@ -0,0 +1,63 @@
+package com.Infiniteloop.TogetherPlay;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.Infiniteloop.TogetherPlay.R;
+
+public class splashActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.antirotate);
+        final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        imageView.startAnimation(animation_2);
+        animation_2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        animation_1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_3);
+                finish();
+                Intent i = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
